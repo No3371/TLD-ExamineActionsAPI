fd852923395a89e9f0c418ad9c268f70a99bb17e
BA <developer@3371.online>
2024-12-17 03:43:58 +0800

    Rename interface method

--- api/Interfaces/IExamineActionRequireLiquid.cs
+++ api/Interfaces/IExamineActionRequireLiquid.cs
@@ -5,6 +5,6 @@ namespace ExamineActionsAPI
 	// Note: Subject being exmained is not condisered a material candidate (only other stacks) even if it has LiquidItem
     public interface IExamineActionRequireLiquid
 	{
-		void GetRequireLiquid (ExamineActionState state, List<MaterialOrProductLiquidConf> liquids);
+		void GetRequiredLiquid (ExamineActionState state, List<MaterialOrProductLiquidConf> liquids);
 	}
 }
