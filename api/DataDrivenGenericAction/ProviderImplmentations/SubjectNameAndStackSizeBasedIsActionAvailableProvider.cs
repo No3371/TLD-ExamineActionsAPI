7c19cbf3b2d594fde2fa23016ad63fa9fc75388a
BA <developer@3371.online>
2024-12-23 14:29:14 +0800

    Redo SubjectNameAndStackSizeBasedIsActionAvailableProvider

--- api/DataDrivenGenericAction/ProviderImplmentations/SubjectNameAndStackSizeBasedIsActionAvailableProvider.cs
+++ api/DataDrivenGenericAction/ProviderImplmentations/SubjectNameAndStackSizeBasedIsActionAvailableProvider.cs
@@ -1,23 +1,22 @@
 // #define VERY_VERBOSE
 using Il2Cpp;
 
-namespace ExamineActionsAPI.DataDrivenGenericAction
+namespace ExamineActionsAPI.DataDrivenGenericAction;
+
+public class SubjectNameAndStackSizeBasedIsActionAvailableProvider : IIsActionAvailableProvider
 {
-    public class SubjectNameAndStackSizeBasedIsActionAvailableProvider : IIsActionAvailableProvider
-    {
 		[MelonLoader.TinyJSON.Include]
-		int? DefaultStackSizeRequired { get; set; }
+		int DefaultStackSizeRequired { get; set; } = 99999;
 		[MelonLoader.TinyJSON.Include]
 		public Dictionary<string, int> Map { get; set;}
 
-        public bool IsActionAvailable(GearItem item)
-        {
+    public bool IsActionAvailable(GearItem item)
+    {
 			int stackSize = item.m_StackableItem?.m_Units ?? 1;
 
 			if (Map.TryGetValue(item.name, out var req))
 				return stackSize >= req;
 			else
-				return stackSize >= (DefaultStackSizeRequired ?? 1);
-        }
+				return stackSize >= DefaultStackSizeRequired;
     }
 }
